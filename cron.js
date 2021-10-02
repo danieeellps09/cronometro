2ccb3deefd0840525eab70283434e93104ed6235
Willian Santos <santoswillc@gmail.com>
2021-10-02 12:50:51 -0300

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,53 +1,53 @@
+let hh = 0; // "let" é uma forma mais moderna de escrever "var"
+let mm = 0;
+let ss = 0;
+let con = 0;
 
-var hh = 0
-var mm = 0 
-var ss = 0
+// "const" define uma constante, uma variável que não pode ser alterada
+const counter = document.querySelector('.counter');
+const startBtn = document.querySelector('#start-button');
+const pauseBtn = document.querySelector('#pause-button');
+const stopBtn = document.querySelector('#stop-button');
 
-var tempo = 1000;
-var cron;
+// Quando o evento "click" for disparado no botão, a função correspondente será chamada
+startBtn.addEventListener('click', start);
+pauseBtn.addEventListener('click', pause);
+stopBtn.addEventListener('click', stop);
 
-//Definindo as três funções principais 
-
-
-
-
-//
+// Definindo as três funções principais 
 function start() {
-    cron = setInterval(() => { timer(); }, tempo)
+    console.log('start');
+    cron = setInterval(() => timer(), 1000);
 } 
 
-
-
-//
 function pause(){
-clearInterval (cron)
+    console.log('pause');
+    clearInterval(cron);
+}
 
+function stop(){
+    console.log('stop');
+    clearInterval(cron);
+    hh = 0;
+    mm = 0;
+    ss = 0;
+    counter.innerText = '00:00:00';
 }
 
 //
-function stop (){
-    clearInterval (cron)
-    hh = 0
-    mm = 0 
-    ss = 0
-    document.getElementsByClassName ('counter').innerText= '00:00:00'
-}
-
-//
-function timer (){
+function timer(){
     ss++ 
     if ( ss == 60) {
         ss = 0
         mm++;
     
-        if (mm == 60){
-         mm = 0 
-         hh++
+        if (mm == 60) {
+            mm = 0 
+            hh++
         }
     }
     
-    var format = (hh<10 ? '0' + hh : hh) + ':' + (mm <10 ? '0' + mm:mm) + ':' + (ss < 10 ? '0' + ss:ss)
-    
-   document.getElementsByClassName ('counter').innerText= format
+    const format = (hh<10 ? '0' + hh : hh) + ':' + (mm <10 ? '0' + mm:mm) + ':' + (ss < 10 ? '0' + ss:ss);
+    counter.innerText= format;
 }
 
